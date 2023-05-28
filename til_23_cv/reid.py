924ef7fe437799ea8c2a2d2e51ed111dedbae4ec
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2023-05-28 12:46:52 +0800

    impl validation logic

--- til_23_cv/reid.py
+++ til_23_cv/reid.py
@@ -1,14 +1,16 @@
 """Suspect Recognition model."""
 
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 import lightning.pytorch as pl
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from sklearn.metrics import silhouette_score
 from torch.optim import Adam
-from torch.optim.lr_scheduler import OneCycleLR
 
+# from torch.optim.lr_scheduler import OneCycleLR
 # from torchvision.ops import sigmoid_focal_loss
 
 __all__ = ["LitImEncoder"]
@@ -40,6 +42,14 @@ class LitImEncoder(pl.LightningModule):
         # TODO: Try focal Loss to deal with class imbalance.
         # loss = sigmoid_focal_loss(logits, tgts, reduction="mean")
 
+        # (embeddings, label) produced during testing.
+        self._eval_embeds: List[np.ndarray] = []
+        self._eval_lbls: List[int] = []
+
+    def forward(self, x: torch.Tensor):
+        """Forward pass."""
+        return self.model(x)
+
     def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
         """Training step.
 
@@ -57,14 +67,16 @@ class LitImEncoder(pl.LightningModule):
         ims, lbls = batch
 
         # BCHW -> BC
-        feats = self.model(ims)
+        feats = self.forward(ims)
         # TODO: Consider label smoothing.
         # B -> BN
         tgts = F.one_hot(lbls.long(), self.nclasses).type_as(feats)
         logits = self.head(feats, tgts)
 
         loss = self.loss(logits, lbls)
-        self.log("train_loss", loss)
+        acc = (logits.argmax(dim=1) == lbls).float().mean()
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log("train_acc", acc, on_step=True, on_epoch=True, prog_bar=True)
         return loss
 
     def configure_optimizers(self):
@@ -75,3 +87,33 @@ class LitImEncoder(pl.LightningModule):
         # change optimizer/scheduler midtraining for multi-stage finetune.
         optimizer = Adam(self.parameters(), lr=lr)
         return optimizer
+
+    def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
+        """Validation step.
+
+        ``batch`` is a tuple of images and class ids. Images should be BCHW and
+        class ids should be B.
+
+        Due to the nature of cluster-based evaluation, the actual validation logic
+        is in ``on_validation_epoch_end``.
+
+        Args:
+            batch (Tuple[torch.Tensor, torch.Tensor]): Batch of images and labels.
+            batch_idx (int): Batch index.
+        """
+        assert self.head is not None and self.nclasses > 0
+        ims, lbls = batch
+        # BCHW -> BC
+        feats = self.forward(ims)
+        for feat, lbl in zip(feats, lbls):
+            self._eval_embeds.append(feat.numpy(force=True))
+            self._eval_lbls.append(int(lbl))
+
+    def on_validation_epoch_end(self):
+        """Actual Validation Logic."""
+        x = np.stack(self._eval_embeds)
+        y = np.array(self._eval_lbls)
+        score: float = silhouette_score(x, y, metric="cosine")  # type: ignore
+        self.log("val_sil_score", score, on_epoch=True, prog_bar=True)
+        self._eval_embeds.clear()
+        self._eval_lbls.clear()
